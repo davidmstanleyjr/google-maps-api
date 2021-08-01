609b53a737574850de296ab79bb7a69d4832dc38
David M. Stanley Jr <davidmstanleyjr@gmail.com>
2021-08-01 07:21:17 -0400

    font

--- app.js
+++ app.js
@@ -1,33 +1,36 @@
-//this establishes the map options
-var mylatlng = { lat: 38.346, lng: -0.4907 };
+var myLatLng = { lat: 38.346, lng: -0.4907 };
 var mapOptions = {
-	center: mylatlng,
+	center: myLatLng,
 	zoom: 7,
 	mapTypeId: google.maps.MapTypeId.ROADMAP,
 };
 
-// this creates the map
+//create map
 var map = new google.maps.Map(document.getElementById("googleMap"), mapOptions);
 
-// this creates an object to be used with the route method and to receive a result for the request.
-var directionService = new google.maps.DirectionsService();
+//create a DirectionsService object to use the route method and get a result for our request
+var directionsService = new google.maps.DirectionsService();
 
-// this displays the route
+//create a DirectionsRenderer object which we will use to display the route
 var directionsDisplay = new google.maps.DirectionsRenderer();
 
+//bind the DirectionsRenderer to the map
 directionsDisplay.setMap(map);
 
+//define calcRoute function
 function calcRoute() {
+	//create request
 	var request = {
 		origin: document.getElementById("from").value,
 		destination: document.getElementById("to").value,
-		travelMode: google.maps.TravelMode.DRIVING, // can change to walking, cycling or transit
+		travelMode: google.maps.TravelMode.DRIVING, //WALKING, BYCYCLING, TRANSIT
 		unitSystem: google.maps.UnitSystem.IMPERIAL,
 	};
 
+	//pass the request to the route method
 	directionsService.route(request, function (result, status) {
 		if (status == google.maps.DirectionsStatus.OK) {
-			//this is for distance and time
+			//Get distance and time
 			const output = document.querySelector("#output");
 			output.innerHTML =
 				"<div class='alert-info'>From: " +
@@ -40,20 +43,22 @@ function calcRoute() {
 				result.routes[0].legs[0].duration.text +
 				".</div>";
 
-			//this displays the route
+			//display route
 			directionsDisplay.setDirections(result);
 		} else {
-			//this deletes the route from the map
+			//delete route from map
 			directionsDisplay.setDirections({ routes: [] });
+			//center map in London
 			map.setCenter(myLatLng);
+
+			//show error message
 			output.innerHTML =
 				"<div class='alert-danger'><i class='fas fa-exclamation-triangle'></i> Could not retrieve driving distance.</div>";
 		}
 	});
 }
 
-// autocomplete for every input
-
+//create autocomplete objects for all inputs
 var options = {
 	types: ["(cities)"],
 };
